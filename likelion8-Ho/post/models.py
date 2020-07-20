5e1c76edd18acd904432126477f8df33c9a3e07b
Ho <ii8858@naver.com>
2020-07-20 21:09:57 +0900

    comment, writer

---
+++ likelion8-Ho/post/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Post(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, default=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # 작성시간
+    updated_at = models.DateTimeField(auto_now=True) # 수정시간
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, default=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # DB에서는 길이제한 유무에 따라서 문자열 필드타입이 달라집니다.
+    # 길이 제한이 없는 문자열을 많이 쓰면 성능이 좋지 않다.
+
+# 유저 가져오기
+
